77e64eecca78807d2e96b4eb6c7ac8640078f57f
Aliaksei Kryzhanouski <87375027+KJI0YH@users.noreply.github.com>
2022-02-17 20:42:30 +0300

    Add files via upload

--- var2.sh
+++ var2.sh
@@ -2,13 +2,18 @@
 catalog=$1
 
 let count=0
-IFS=$'\n'
-list=$(find "$catalog" -type f)
-for i in $list
-do	
-	let "count+=1"
-	find "$i" -printf "%M %s %p\n"
-done
+if [ ! -d "$catalog" ]
+then
+	echo "The '"$catalog"' is not a directory"
+else
+	IFS=$'\n'
+	list=$(find "$catalog" -type f)
+	for i in $list
+	do	
+		let "count+=1"
+		find "$i" -printf "%M %s %p\n"
+	done
+fi
 echo  "Listed "$count" files"
 
 
