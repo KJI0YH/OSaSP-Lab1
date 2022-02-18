a1eed8728e764bb7184a0a85229e778866f0f0ea
Aliaksei Kryzhanouski <87375027+KJI0YH@users.noreply.github.com>
2022-02-18 09:23:52 +0300

    Add files via upload

--- extensions.sh
+++ extensions.sh
@@ -5,10 +5,13 @@ extension=$3
 
 if [ $# -lt 3 ]
 then
-	echo "Invalid number of parameters"	
+{
+
+	echo "Invalid number of parameters"
 	echo "\$1 - out file name"
 	echo "\$2 - directory to search in"
 	echo "\$3 - extension to find"
+}>&2
 elif [ ! -d $catalog ]
 then
 	echo "The file "$catalog" is not a directory">&2
